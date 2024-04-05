b375fa16be55a09ea1715a193b6610a5aed6685a
Evgeniy Filippov <6479556@gmail.com>
2024-04-05 15:57:15 +0300

    feat: lesson 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,70 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+  home: UserPanel(),
+));
 
-class MyApp extends StatelessWidget {
+class UserPanel extends StatefulWidget {
+  const UserPanel({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _UserPanelState();
+}
+
+class _UserPanelState extends State<UserPanel> {
+
+  int _count = 0;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.deepOrangeAccent),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My test App'),
-          centerTitle: true,
-          backgroundColor: Colors.deepOrangeAccent,
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
+      appBar: AppBar(
+        title: Text('app Bar title'),
+        centerTitle: true,
+        backgroundColor: Colors.black45,
+      ),
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+                children: [
+                  Padding(padding: EdgeInsets.only(top: 30),),
+                  Text('John Doe', style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white
+                  ),),
+                  Padding(padding: EdgeInsets.only(top: 10),),
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/image1.png'),
+                    radius: 50,
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 10),),
+                  Row(
+                    children: [
+                      Icon(Icons.mail_outline, size: 24,),
+                      Padding(padding: EdgeInsets.only(left: 10),),
+                      Text('test@google.com', style: TextStyle(color: Colors.white))
+                    ],
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 10),),
+                  Text('Count: $_count', style: TextStyle(color: Colors.white))
+                ]
+            )
+          ],
         ),
-        body:
-            Container(
-              color: Colors.green,
-              child: Text('container'),
-              margin: EdgeInsets.symmetric(horizontal: 10.5, vertical: 50.0),
-              padding: EdgeInsets.all(20.5),
-            ),
-            // Image(
-            //   image: AssetImage('assets/image1.png'),
-            // )
-            // ElevatedButton.icon(
-            //   icon: Icon(Icons.ac_unit_rounded),
-            //   onPressed: () {  },
-            //   label: Text('El button'),
-            // )
-            // TextButton.icon(
-            //     style: ButtonStyle(
-            //       foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-            //       backgroundColor: MaterialStateProperty.all<Color>(Colors.deepOrangeAccent)
-            //     ),
-            //     onPressed: () {},
-            //     icon: Icon(Icons.ac_unit_sharp),
-            //     label: Text('Press'),
-            // ),
-            // Icon(Icons.settings, size: 45, color: Colors.amber),
-
-        floatingActionButton: FloatingActionButton(
-          onPressed: () { 
-            print('clicked');
-          },
-          child: Text('Press me'),
-          backgroundColor: Colors.deepOrangeAccent,
-        ),
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.plus_one),
+        backgroundColor: Colors.amber,
+        onPressed: () {
+          setState(() {
+            _count++;
+          });
+        },
+      ),
     );
   }
 }
+
